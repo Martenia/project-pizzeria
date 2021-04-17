553c37d4accd7b111841b4e656db16f8493d34c8
Marta Nitek <marta.nitek@onet.pl>
2021-04-17 20:49:09 +0200

    Użycie Widgetów DatePicker i HourPicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {templates, select} from '../settings.js';
 import AmountWidget from '/js/components/AmountWidget.js';
+import DatePicker from '/js/components/DatePicker.js';
+import HourPicker from '/js/components/HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -23,6 +25,8 @@ class Booking {
 
     thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets() {
@@ -30,6 +34,8 @@ class Booking {
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
 
